31e8f29c9148bb66f17adea4fb91c9054a4d5c89
crotch-watch <naps_08_bay@icloud.com>
2025-04-03 00:34:51 +0530

    refac: shifted routing and controlling logic into separate files

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-const fs = require('node:fs')
-
 const express = require('express')
 const morgan = require('morgan')
 
-const TOURS_PATH = `./dev-data/data/tours-simple.json`
-const toursData = JSON.parse(fs.readFileSync(TOURS_PATH).toString())
-const TOURS = '/api/v1/tours'
+const tourRouter = require('./routes/tour.routes')
+const userRouter = require('./routes/user.routes')
 
-const USERS = '/api/v1/users'
-
-const INTERNAL_SERVER_ERROR = 500
+const { TOURS, USERS } = require('./constants')
 
 const app = express()
 
@@ -24,136 +19,6 @@ app.use((req, res, next) => {
   next()
 })
 
-// handlers
-const getTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: toursData.length,
-    data: { tours: toursData },
-  })
-}
-
-const getTour = (req, res) => {
-  const id = +req.params.id
-
-  if (id > toursData.length) {
-    return res.status(404).json({
-      status: 'error',
-      message: 'No ID found.',
-    })
-  }
-
-  const reqTour = toursData.find((tour) => tour.id === id)
-
-  res.status(200).json({
-    requestedAt: req.reqestedAt,
-    status: 'success',
-    data: {
-      tour: reqTour,
-    },
-  })
-}
-
-const addTour = (req, res) => {
-  const { id: lastTourId } = toursData[toursData.length - 1]
-  const newTour = { id: lastTourId + 1, ...req.body }
-  toursData.push(newTour)
-  const newToursJson = JSON.stringify(toursData)
-
-  fs.writeFile(TOURS_PATH, newToursJson, (fileWriteError) => {
-    if (fileWriteError) console.log(fileWriteError)
-    else
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      })
-  })
-}
-
-const updateTour = (req, res) => {
-  const id = +req.params.id
-  const { body } = req
-
-  if (id > toursData.length) {
-    return res.status(404).json({
-      status: 'error',
-      message: 'No ID found.',
-    })
-  }
-
-  const updateTour = toursData.find((tour) => tour.id === id)
-
-  Object.entries(body).forEach(([key, value]) => {
-    updateTour[key] = value
-  })
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: updateTour,
-    },
-  })
-}
-
-const deleteTour = (req, res) => {
-  const id = +req.params.id
-
-  if (id > toursData.length) {
-    return res.status(404).json({
-      status: 'error',
-      message: 'No ID found.',
-    })
-  }
-
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  })
-}
-
-const getAllUsers = (req, res) => {
-  res.status(INTERNAL_SERVER_ERROR).json({
-    status: 'error',
-    message: 'route not found',
-  })
-}
-
-let createUser = getAllUsers,
-  updateUser = getAllUsers,
-  getUser = getAllUsers,
-  deleteUser = getAllUsers
-
-// Routers
-const tourRouter = express.Router()
-const userRouter = express.Router()
-
 app.use(TOURS, tourRouter).use(USERS, userRouter)
 
-// routes
-// tours@start
-tourRouter.route('/').get(getTours).post(addTour)
-
-// // sequential middleware exec demo
-app.use((res, req, next) => {
-  // since middleware are invoked sequentially if tours don't have an id
-  // then this middleware won't have any effect
-  res.requiresId = true
-  next()
-})
-
-tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour)
-// tours@end
-
-// user@start
-userRouter.route('/').get(getAllUsers).post(createUser)
-userRouter.route('/:id').get(getUser).delete(deleteUser).patch(updateUser)
-// user@end
-
-// server config and init.
-const PORT = 3000
-app.listen(PORT, (startupError) => {
-  if (startupError) console.log('Error starting server :', startupError)
-  else console.log('Listening on port ', PORT)
-})
+module.exports = app
