d37a821d5f21a4439e1b1a2849f83e3152f7a324
crotch-watch <naps_08_bay@icloud.com>
2025-05-12 23:48:28 +0530

    add: simple mongoose setup

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
-const app  = require('./app')
+const app = require('./app')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
 const { PORT } = require('./constants')
+
+dotenv.config({ path: './.env' })
+
+const USER = process.env.db_user
+const PASSWORD = process.env.db_password
+const DB = process.env.mongo_connection_string
+  .replace('<USER>', USER)
+  .replace('<PASSWORD>', PASSWORD)
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(console.log)
 
 // server config and init.
 app.listen(PORT, (startupError) => {
