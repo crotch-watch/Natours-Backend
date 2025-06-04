b9513438e3416100e3cc2372dff2878c49f6a11a
crotch-watch <naps_08_bay@icloud.com>
2025-06-04 14:11:07 +0530

    add: http response status codes

--- constants.js
+++ constants.js
@@ -6,3 +6,10 @@ exports.TOURS = '/api/v1/tours'
 exports.USERS = '/api/v1/users'
 exports.INTERNAL_SERVER_ERROR = 500
 exports.PORT = 3000
+
+exports.STATUS = {
+  OK: 200,
+  CREATED: 201,
+  NO_CONTENT: 204,
+  BAD_REQUEST: 400
+}
